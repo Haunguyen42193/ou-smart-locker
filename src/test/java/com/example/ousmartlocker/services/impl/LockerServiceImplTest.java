054d2a221ec1f9d7d8bb7163b005ed216796ae02
Haunguyen42193 <haunguyen42193@gmail.com>
2024-03-06 10:55:09 +0700

    test get all locker

--- src/test/java/com/example/ousmartlocker/services/impl/LockerServiceImplTest.java
+++ src/test/java/com/example/ousmartlocker/services/impl/LockerServiceImplTest.java
@@ -1,24 +1,46 @@
 package com.example.ousmartlocker.services.impl;
 
+import com.example.ousmartlocker.dto.OuSmartLockerResp;
+import com.example.ousmartlocker.model.Locker;
+import com.example.ousmartlocker.repository.LockerRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class LockerServiceImplTest {
 
-    @Test
-    void addlocker() {
+    @Mock
+    LockerRepository lockerRepository;
 
-    }
+    @InjectMocks
+    LockerServiceImpl lockerService;
+
 
     @Test
     void getAlllocker() {
+        List<Locker> lockers = new ArrayList<>();
+        lockers.add(new Locker(/*thông tin locker 1*/));
+        lockers.add(new Locker(/*thông tin locker 2*/));
+        // ...
 
+        Mockito.when(lockerRepository.findAll()).thenReturn(lockers);
+
+        // Gọi phương thức cần kiểm tra
+        OuSmartLockerResp result = lockerService.getAlllocker();
+
+        // Kiểm tra kết quả
+        assertEquals(HttpStatus.OK, result.getStatus());
+        assertEquals("Get all locker successful", result.getMessage());
+        assertEquals(lockers, result.getData());
     }
 
-    @Test
-    void registerLocker() {
-    }
 }
