68e69e12bdc24637dd306a699969853eb5acb047
Haunguyen42193 <haunguyen42193@gmail.com>
2024-06-08 19:20:00 +0700

    update cors

--- src/main/java/com/example/ousmartlocker/config/SecurityConfig.java
+++ src/main/java/com/example/ousmartlocker/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 @Configuration
@@ -31,7 +32,8 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(httpSecurityCorsConfigurer -> new CorsConfiguration().applyPermitDefaultValues())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests.requestMatchers(HttpMethod.GET)
                         .permitAll()
                         .requestMatchers("/api/auth/**")
