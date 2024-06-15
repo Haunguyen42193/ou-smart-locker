3d6da11e8ca8ff9223da526d354132e2a86bfcf8
Haunguyen42193 <haunguyen42193@gmail.com>
2024-06-15 16:36:39 +0700

    fix admin api

--- src/main/java/com/example/ousmartlocker/repository/LockerRepository.java
+++ src/main/java/com/example/ousmartlocker/repository/LockerRepository.java
@@ -1,6 +1,7 @@
 package com.example.ousmartlocker.repository;
 
 import com.example.ousmartlocker.model.Locker;
+import com.example.ousmartlocker.model.LockerLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,5 +13,7 @@ public interface LockerRepository extends JpaRepository<Locker, Long> {
 
     @Query("select l from locker l where l.isOccupied = false and l.lockerLocation.locationId = :locationId")
     List<Locker> findByIsOccupiedFalseAndLockerLocation(Long locationId);
+
+    List<Locker> findAllByLockerLocation_LocationId(Long locationId);
 
 }
--- src/main/java/com/example/ousmartlocker/services/impl/LocationServiceImpl.java
+++ src/main/java/com/example/ousmartlocker/services/impl/LocationServiceImpl.java
@@ -8,15 +8,21 @@ import com.example.ousmartlocker.exception.LockerNotFoundException;
 import com.example.ousmartlocker.model.Locker;
 import com.example.ousmartlocker.model.LockerLocation;
 import com.example.ousmartlocker.repository.LockerLocationRepository;
+import com.example.ousmartlocker.repository.LockerRepository;
 import com.example.ousmartlocker.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LocationServiceImpl implements LocationService {
     @Autowired
     private LockerLocationRepository lockerLocationRepository;
+    @Autowired
+    private LockerRepository lockerRepository;
+
     @Override
     public OuSmartLockerResp getAllLocation() {
         return OuSmartLockerResp.builder().status(HttpStatus.OK).message("Get all location successful").data(lockerLocationRepository.findAll()).build();
@@ -29,9 +35,11 @@ public class LocationServiceImpl implements LocationService {
 
     @Override
     public OuSmartLockerResp deleteLocation(long id) {
-        LockerLocation locker = lockerLocationRepository.findById(id)
+        LockerLocation location = lockerLocationRepository.findById(id)
                 .orElseThrow(() -> new LockerLocationNotFoundException("Location not found"));
-        lockerLocationRepository.delete(locker);
+        List<Locker> lockers = lockerRepository.findAllByLockerLocation_LocationId(location.getLocationId());
+        lockerRepository.deleteAll(lockers);
+        lockerLocationRepository.delete(location);
         return OuSmartLockerResp.builder().status(HttpStatus.NO_CONTENT).message("Success").data(null).build();
     }
 
