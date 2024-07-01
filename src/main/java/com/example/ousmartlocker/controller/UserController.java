9b6cf8578b045a51463a85415ab820712937ba7c
Võ Hữu Nghĩa <nghiabongda2@gmail.com>
2024-07-01 11:10:04 +0700

    test cicd

--- src/main/java/com/example/ousmartlocker/controller/UserController.java
+++ src/main/java/com/example/ousmartlocker/controller/UserController.java
@@ -55,6 +55,7 @@ public class UserController {
     }
 
     @PostMapping("/user-info/role")
+    @PreAuthorize("hasRole('ADMIN')")
     public OuSmartLockerResp updateUserInfo(@RequestBody UpdateUserInfoDto updateUserInfoDto) {
         return userService.updateUserInfo(updateUserInfoDto);
     }
